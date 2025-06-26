4671802d5ed0e41c2f6d5815c80954e77cf44f77
Thivagard7305 <cloudforge1408@gmail.com>
2025-06-26 11:28:19 +0530

    Add files via upload

---
+++ Azure_DevOps_project/terraform/main.tf
@@ -0,0 +1,45 @@
+provider "azurerm" {
+  features {}
+
+  subscription_id = "${var.subscription_id}"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = var.acr_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.aks_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "${var.aks_name}-dns"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+resource "azurerm_role_assignment" "aks_acr_pull" {
+  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  role_definition_name            = "AcrPull"
+  scope                           = azurerm_container_registry.acr.id
+}
---
+++ Azure_DevOps_project/terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "subscription_id" {
+  default = "9558e3f0-5527-4f1a-9d10-c9745887163b"
+}
+
+variable "location" {
+  default = "East US"
+}
+
+variable "resource_group_name" {
+  default = "MyGreensApp-RG"
+}
+
+variable "acr_name" {
+  default = "mygreensappacr"
+}
+
+variable "aks_name" {
+  default = "mygreensappaks"
+}
